09aac2e0bbcf3491050e3ee0381403f10173b840
Toxanchiq <ivan.petrov@student.com>
2025-04-18 07:18:12 +0600

    Your commit message

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class git --version
-Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.*;
+import java.util.Scanner;
+
+public class UserManagementSystem {
+    private static final String DATA_FILE = "users_data.json";
+    private static JSONArray usersData = new JSONArray();
+
+    public static void main(String[] args) {
+        loadData();
+        showMenu();
+    }
+
+    // Function to load data from JSON file
+    private static void loadData() {
+        File file = new File(DATA_FILE);
+        if (file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                StringBuilder json = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    json.append(line);
+                }
+                usersData = new JSONArray(json.toString());
+            } catch (IOException e) {
+                System.out.println("Error loading data: " + e.getMessage());
+            }
         }
     }
-}
+
+    // Function to save data to JSON file
+    private static void saveData() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(DATA_FILE))) {
+            writer.write(usersData.toString(4));
+        } catch (IOException e) {
+            System.out.println("Error saving data: " + e.getMessage());
+        }
+    }
+
